7b9582fbd2655b46eb808c230893f3a59eb3d068
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-02-28 09:29:28 +0100

    1 - New resource definition

---
+++ src/Entity/Product/ProductScarcity.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Product;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_product_scarcity")
+ */
+final class ProductScarcity implements ResourceInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $productVariantName;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $quantityLeft;
+
+    /**
+     * @var \DateTimeInterface
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $since;
+
+    public function __construct()
+    {
+        $this->since = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getProductVariantName(): ?string
+    {
+        return $this->productVariantName;
+    }
+
+    public function setProductVariantName(?string $productVariantName): void
+    {
+        $this->productVariantName = $productVariantName;
+    }
+
+    public function getQuantityLeft(): ?int
+    {
+        return $this->quantityLeft;
+    }
+
+    public function setQuantityLeft(?int $quantityLeft): void
+    {
+        $this->quantityLeft = $quantityLeft;
+    }
+
+    public function getSince(): \DateTimeInterface
+    {
+        return $this->since;
+    }
+}
